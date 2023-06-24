a477df1429ab70798a5095f1be1f7487e6a21768
Ashrut Panchal <ashrutpanchal111@gmail.com>
2023-06-24 16:57:13 +0530

    Added update routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,15 @@
+# frozen_string_literal: true
+
 class ApplicationController < ActionController::API
-    include ActionController::Cookies
-    before_action :authorized_employee
+  include ActionController::Cookies
+  before_action :authorized_employee
 
-    def current_employee
-        employee = Employee.find_by(id: session[:employee_id])
-        employee
-    end
-
-    def authorized_employee
-        render json: {errors: "Not Authorized"}, status: :unauthorized unless current_employee
-    end
+  def current_employee
+    employee = Employee.find_by(id: session[:employee_id])
+    employee
   end
+
+  def authorized_employee
+    render json: { errors: "Not Authorized" }, status: :unauthorized unless current_employee
+  end
+end
--- app/controllers/inventories_controller.rb
+++ app/controllers/inventories_controller.rb
@@ -1,7 +1,17 @@
 # frozen_string_literal: true
 
 class InventoriesController < ApplicationController
+  before_action :find_employee, only: :update
   def assign
     # TODO: Reduce Quantity from the sender and increase from the reciever 
+  end
+
+  def update
+    # TODO: Update the Prducts-Quantities for that particular employee 
+  end
+
+  private
+  def find_employee
+    @employee = Employee.find_by(id: params[:id] || session[:employee_id])    
   end
 end
