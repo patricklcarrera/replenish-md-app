958d96a7345fd9e491bbbc8eb8d85a8d897e9ce9
Ashrut Panchal <ashrutpanchal111@gmail.com>
2023-07-11 01:25:54 +0530

    Updating the Employee every time on the reset password

--- client/src/components/CustomModal.js
+++ client/src/components/CustomModal.js
@@ -236,7 +236,6 @@ function CustomModal(props) {
       });
   };
 
-  // product&.first, product&.second, product&.third, (product.second.to_i * product.third.to_i)
   return (
     <>
       <Modal
@@ -272,54 +271,61 @@ function CustomModal(props) {
                 </div>
               </div>
 
-              <div className=" border rounded-sm p-2 mb-4 flex justify-content-center">
-                <div>
-                  <p><b>Products</b></p>
-                  <thead>
-                    <tr>
-                      <th>Product</th>
-                      <th>Quantity</th>
-                      <th>Price</th>
-                      <th>Total Price</th>
-                    </tr>
-                  </thead>
-                  {invoiceData?.products_hash?.products.map((product) => (
-                    <tbody>
-                      <tr>
-                        <td>{product[0]}</td>
-                        <td>{product[1]}</td>
-                        <td>{product[2]}</td>
-                        <td>{+(product[1] * product[2])}</td>
-                      </tr>
-                    </tbody>
-                  ))}
-                </div>
-              </div>
+              {invoiceData?.products_hash?.products.length > 0 &&
+                (<div className=" border rounded-sm p-2 mb-4 flex justify-content-center">
+                  <div>
+                    <p><b>Products</b></p>
+                    <table>
+                      <thead>
+                        <tr>
+                          <th>Product</th>
+                          <th>Quantity</th>
+                          <th>Price</th>
+                          <th>Total Price</th>
+                        </tr>
+                      </thead>
+                      {invoiceData?.products_hash?.products.map((product) => (
+                        <tbody>
+                          <tr>
+                            <td>{product[0]}</td>
+                            <td>{product[1]}</td>
+                            <td>{product[2]}</td>
+                            <td>{+(product[1] * product[2])}</td>
+                          </tr>
+                        </tbody>
+                      ))}
+                    </table>
+                  </div>
+                </div>)
+              }
 
-              <div className=" border rounded-sm p-2 mb-4 flex justify-content-center">
-                <div>
-                  <p><b>Retail Products</b></p>
-
-                  <thead>
-                    <tr>
-                      <th scope="col">Product</th>
-                      <th scope="col">Quantity</th>
-                      <th scope="col">Price</th>
-                      <th scope="col">Total Price</th>
-                    </tr>
-                  </thead>
-                  {invoiceData?.products_hash?.retail_products.map((product) => (
-                    <tbody>
-                      <tr>
-                        <td>{product[0]}</td>
-                        <td>{product[1]}</td>
-                        <td>{product[2]}</td>
-                        <td>{+(product[1] * product[2])}</td>
-                      </tr>
-                    </tbody>
-                  ))}
-                </div>
-              </div>
+              {invoiceData?.products_hash?.retail_products.length > 0 &&
+                (<div className=" border rounded-sm p-2 mb-4 flex justify-content-center">
+                  <div>
+                    <p><b>Retail Products</b></p>
+                    <table>
+                      <thead>
+                        <tr>
+                          <th scope="col">Product</th>
+                          <th scope="col">Quantity</th>
+                          <th scope="col">Price</th>
+                          <th scope="col">Total Price</th>
+                        </tr>
+                      </thead>
+                      {invoiceData?.products_hash?.retail_products.map((product) => (
+                        <tbody>
+                          <tr>
+                            <td>{product[0]}</td>
+                            <td>{product[1]}</td>
+                            <td>{product[2]}</td>
+                            <td>{+(product[1] * product[2])}</td>
+                          </tr>
+                        </tbody>
+                      ))}
+                    </table>
+                  </div>
+                </div>)
+              }
 
               <div className="border rounded-sm p-2 mb-4 flex justify-content-around">
                 <div>
--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -2,40 +2,41 @@ import React, {useDebugValue, useState} from 'react'
 import {useNavigate} from 'react-router-dom'
 import { Form, Button } from 'react-bootstrap'
 import {toast} from "react-toastify";
-function Login({updateEmployee}) {
-    const loginState = {
-      email: '',
-      password: ''
-    };
 
-    const [errors, setErrors] = useState([])
-    const [formData, setFormData] = useState(loginState);
-    const navigate = useNavigate();
-  
-    const handleSubmit = (e) => {
-      e.preventDefault()
-      fetch('/api/login', {
-          method: 'POST',
-          headers:{'Content-Type': 'application/json'},
-          body:JSON.stringify(formData)
-      })
-      .then(res => {
-          if(res.ok){
-              res.json().then(user => {
-                  updateEmployee(user);
-                  toast.success('Successfully Logged In');
-                  navigate('/myprofile')
-              })
-          } else if(res.status == 302)  {
-            res.json().then(user => {
-              navigate('/resetPassword')
-            })
-          } else {
-              res.json().then(json => setErrors(json.errors))
-              toast.error('Failed to Log In');
-              setFormData(loginState)
-          }
-      })
+function Login({updateEmployee}) {
+  const loginState = {
+    email: '',
+    password: ''
+  };
+
+  const [errors, setErrors] = useState([])
+  const [formData, setFormData] = useState(loginState);
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    fetch('/api/login', {
+      method: 'POST',
+      headers:{'Content-Type': 'application/json'},
+      body:JSON.stringify(formData)
+    })
+    .then(res => {
+      if(res.ok){
+        res.json().then(user => {
+          updateEmployee(user);
+          toast.success('Successfully Logged In');
+          navigate('/myprofile')
+        })
+      } else if(res.status == 302)  {
+        res.json().then(user => {
+          navigate('/resetPassword')
+        })
+      } else {
+        res.json().then(json => setErrors(json.errors))
+        toast.error('Failed to Log In');
+        setFormData(loginState)
+      }
+    })
   }
   const handleChange = (e) => {
     const { name, value } = e.target
@@ -45,32 +46,31 @@ function Login({updateEmployee}) {
   const loginForm = 
     <Form onSubmit={handleSubmit}>
     <Form.Group >
-        <Form.Label>Email</Form.Label>
-        <Form.Control name="email" type="text" required value={formData.email} placeholder="Enter your email" onChange={handleChange}/>
+      <Form.Label>Email</Form.Label>
+      <Form.Control name="email" type="text" required value={formData.email} placeholder="Enter your email" onChange={handleChange}/>
     </Form.Group>
     <br />
     <Form.Group>
-        <Form.Label>Password</Form.Label>
-        <Form.Control name="password" type="password" required value={formData.password} placeholder="Enter password" onChange={handleChange}/>
+      <Form.Label>Password</Form.Label>
+      <Form.Control name="password" type="password" required value={formData.password} placeholder="Enter password" onChange={handleChange}/>
     </Form.Group>
     <br />
     <Form.Group >
-        <Button type="submit">Login</Button>
+      <Button type="submit">Login</Button>
     </Form.Group>
   </Form>
   
-    return (
-      <div className="min-h-screen flex items-center justify-center bg-gray-100">
-        <div className="max-w-md w-full space-y-8 p-8 bg-white shadow-lg rounded-lg">
-          <div>
-            <h2 className="text-center text-3xl font-extrabold text-gray-900">Sign in to ReplenishMD</h2>
-            {loginForm}
-          </div>
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <div className="max-w-md w-full space-y-8 p-8 bg-white shadow-lg rounded-lg">
+        <div>
+          <h2 className="text-center text-3xl font-extrabold text-gray-900">Sign in to ReplenishMD</h2>
+          {loginForm}
         </div>
       </div>
-    )
+    </div>
+  )
 }
 
 export default Login
-
 
